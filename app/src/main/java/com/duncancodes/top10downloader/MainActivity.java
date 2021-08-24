7ef9e333612e77b8f95e1e3628532261add8ad1a
Duncan Skilton <the_skiltons@hotmail.com>
2021-08-24 16:02:49 +0100

    created nested class AsyncTask

--- app/src/main/java/com/duncancodes/top10downloader/MainActivity.java
+++ app/src/main/java/com/duncancodes/top10downloader/MainActivity.java
@@ -2,6 +2,7 @@ package com.duncancodes.top10downloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //first parameter is URL, second parameter is for progress bar, last parameter is type of result
+    private class DownloadData extends AsyncTask<String, Void, String>{
+
+
+
     }
 }
