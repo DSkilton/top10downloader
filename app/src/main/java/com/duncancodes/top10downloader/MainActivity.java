62033b11fddbdef34b441727e02f3846d1494f45
Duncan Skilton <the_skiltons@hotmail.com>
2021-08-27 16:11:07 +0100

    added log cat

--- app/src/main/java/com/duncancodes/top10downloader/MainActivity.java
+++ app/src/main/java/com/duncancodes/top10downloader/MainActivity.java
@@ -4,25 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: starting Asynctask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL goes here");
+        Log.d(TAG,"onCreate: done");
+
     }
 
     //first parameter is URL, second parameter is for progress bar, last parameter is type of result
     private class DownloadData extends AsyncTask<String, Void, String>{
+        private static final String TAG = "DownloadData";
+
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is " + s);
         }
 
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            Log.d(TAG, "doInBackground: Starts with " + strings[0]);
+            return "doInBackground completed.";
         }
     }
 }
